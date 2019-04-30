86008078015dad001e034710762c43201bb6ed2b
Artem Chugainov <achugainov@lineate.com>
2019-04-30 13:56:21 +0600

    add logic

--- src/index.js
+++ src/index.js
@@ -1,69 +1,91 @@
 import "./scss/main.scss";
 
 $(document).ready(() => {
-  const sliders = [
-    {
-      value: 100,
-      spent: 20,
-      min: "0",
-      max: "200"
+  const channels = {
+    currentBudget: $("#current-budget")[0],
+    newBudget: $("#new-budget")[0],
+    sliders: [
+      {
+        value: 100,
+        spent: 20,
+        min: "0",
+        max: "200"
+      },
+      {
+        value: 200,
+        spent: 0,
+        min: "0",
+        max: "200"
+      },
+      {
+        value: 50,
+        spent: 40,
+        min: "0",
+        max: "200"
+      }
+    ],
+
+    initSliders: function(sliders) {
+      $("#sliders")[0].innerHTML = "";
+
+      sliders.forEach((slider, index) => {
+        const sliderHtml = $(`
+            <div class="slidecontainer">
+              <input
+                id="ch-${index}"
+                type="range"
+                min="${slider.spent || 0}"
+                max="${this.currentBudget.value}"
+                value="${slider.value}"
+                class="slider"
+              />
+              <span id="ch-s-${index}" class="value"></span>
+              <span id="ch-o-${index}" class="value"></span>
+            </div>`);
+
+        $("#sliders")[0].appendChild(sliderHtml[0]);
+      });
+
+      return sliders.map((slider, index) => {
+        let updatedSlider = { ...{}, ...{ slider } };
+        updatedSlider.el = document.getElementById(`ch-${index}`);
+        updatedSlider.outputEl = document.getElementById(`ch-o-${index}`);
+        updatedSlider.spentEl = document.getElementById(`ch-s-${index}`);
+        updatedSlider.outputEl.innerHTML = updatedSlider.el.value;
+        updatedSlider.spentEl.innerHTML = this.sliders[index].spent;
+        updatedSlider.el.oninput = e => {
+          this.sliders[index].value = e.currentTarget.value;
+          updatedSlider.outputEl.innerHTML = e.currentTarget.value;
+        };
+      });
     },
-    {
-      value: 200,
-      spent: 0,
-      min: "0",
-      max: "200"
-    },
-    {
-      value: 50,
-      spent: 50,
-      min: "0",
-      max: "200"
+
+    newValues: function(newValue) {
+      const currentBudgetValue = this.currentBudget.value;
+      const newBudgetValue = newValue.currentTarget.value;
+
+      if (newBudgetValue > currentBudgetValue) {
+        const newValues = this.sliders.forEach(slider => {
+          const newVal = (slider.value / currentBudgetValue) * newBudgetValue;
+          slider.value = newVal;
+        });
+      } else {
+        const newValues = this.sliders.forEach(slider => {
+          const newVal =
+            ((slider.value - slider.spent) / currentBudgetValue) *
+            newBudgetValue;
+          slider.value = newVal < slider.spent ? slider.spent : newVal;
+        });
+      }
+
+      this.currentBudget.value = newValue.currentTarget.value;
+      this.initSliders(this.sliders);
     }
-  ];
-
-  function initSliders(sliders) {
-    sliders.forEach((slider, index) => {
-      const sliderHtml = $(`
-          <div class="slidecontainer">
-            <input
-              id="ch-${index}"
-              type="range"
-              min="${slider.min}"
-              max="${currentBudget}"
-              value="50"
-              class="slider"
-            />
-            <span id="ch-o-${index}" class="value"></span>
-          </div>`);
-
-      $("#sliders")[0].appendChild(sliderHtml[0]);
-    });
-
-    return sliders.map((slider, index) => {
-      let updatedSlider = { ...{}, ...{ slider } };
-      updatedSlider.el = document.getElementById(`ch-${index}`);
-      updatedSlider.outputEl = document.getElementById(`ch-o-${index}`);
-      updatedSlider.outputEl.innerHTML = updatedSlider.el.value;
-      updatedSlider.el.oninput = function() {
-        const newValue = validate(this.value, index);
-        updatedSlider.outputEl.innerHTML = this.value;
-      };
-    });
-  }
-
-  const currentBudget = $("#current-budget")[0].value;
-  const newBudget = $("#new-budget")[0].value;
+  };
 
   //   const maxRange = Math.ceil(currentBudget / sliders.length);
 
-  const inicializedSliders = initSliders(sliders, currentBudget);
+  const inicializedSliders = channels.initSliders(channels.sliders);
 
-  function validate(value, index) {
-    console.log(index);
-    sliders[index].value = value;
-
-    // logic
-    return value;
-  }
+  channels.newBudget.oninput = channels.newValues.bind(channels);
 });
